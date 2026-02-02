eade6d9919af5dc1327b63c540d36d1466678645
SL_1056 <142105326+sl1056@users.noreply.github.com>
2026-02-02 11:47:44 +0300

    add pvp part

--- backend/pvp/models.py
+++ backend/pvp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuizRoom(models.Model):
+    room_id = models.CharField(max_length=100, unique=True)
+    player1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='p1')
+    player2 = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='p2')
+    current_question_index = models.IntegerField(default=0)
+    score_p1 = models.IntegerField(default=0)
+    score_p2 = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
