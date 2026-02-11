74476f461086ad8aa3444bdcc9e12a5f6938f8a1
04Ash04 <sm7135971@gmail.com>
2026-02-12 03:36:51 +0800

    date return to the profile
    added the date return to the profile

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -14,6 +14,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             'rating',
             'solved_tasks',
             'correct_answers',
+            'created_at',
         )
 
 class RegisterSerializer(serializers.ModelSerializer):
