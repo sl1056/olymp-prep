d83cf0cdceaa529e83b7e33f6f632c2bce6ee2bf
bentalin <halmanovmihsa@gmail.com>
2026-02-11 22:54:29 +0300

    Update ProfileView.vue

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <head>
+      <head>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     </head>
+  <div>
     <HeaderEnter />
     
     <div class="profile-page">
