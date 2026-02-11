48d453aaa376fd481aaaf6eab91e7f999f10dd99
04Ash04 <sm7135971@gmail.com>
2026-02-12 03:37:38 +0800

    correct ans in task catalog
    added the return of the correct answer to the task catalog

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('id', 'subject', 'topic', 'difficulty', 'text', 'hints')
+        fields = ('id', 'subject', 'topic', 'difficulty', 'text', 'hints', 'correct_answer')
